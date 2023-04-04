c7aaed3a85239f5073e9e2a0705b67e6b6d3f544
kolaT1 <113561175+kolaT1@users.noreply.github.com>
2023-04-04 09:54:35 -0400

    Update Main.java

--- src/bruteforce/Main.java
+++ src/bruteforce/Main.java
@@ -2,7 +2,8 @@ package bruteforce;
 
 import java.util.*;
 import java.io.*;
-public class Main{
+
+public class Main {
     public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
         Scanner input = new Scanner(System.in);
@@ -18,22 +19,28 @@ public class Main{
         int numVars = 0;
         ArrayList<ArrayList<Integer>> clauses = new ArrayList<>();
         Scanner fileInput = new Scanner(file);
+        ArrayList<Integer> currentClause = new ArrayList<>();
         while (fileInput.hasNextLine()) {
             String line = fileInput.nextLine();
-            if (line.startsWith("c")) {
+            if (line.trim().isEmpty() || line.startsWith("c")) {
                 continue;
             } else if (line.startsWith("p")) {
                 String[] parts = line.split(" ");
                 numVars = Integer.parseInt(parts[2]);
             } else {
                 String[] parts = line.split(" ");
-                ArrayList<Integer> clause = new ArrayList<>();
                 for (String part : parts) {
-                    if (!part.equals("0")) {
-                        clause.add(Integer.parseInt(part));
+                    if (part.trim().isEmpty()) {
+                        continue;
+                    }
+                    int literal = Integer.parseInt(part);
+                    if (literal == 0) {
+                        clauses.add(currentClause);
+                        currentClause = new ArrayList<>();
+                    } else {
+                        currentClause.add(literal);
                     }
                 }
-                clauses.add(clause);
             }
         }
         fileInput.close();
@@ -43,27 +50,26 @@ public class Main{
         boolean satisfiable = generateAssignments(clauses, 0, assignment);
         if (satisfiable) {
             System.out.println("satisfiable");
-
         } else {
             System.out.println("unsatisfiable");
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Runtime: " + (endTime - startTime) + " milliseconds");
-
+        System.out.println("Runtime: " + (endTime - startTime) / (1000 * 60) + " mins");
     }
 
-    //  recursion
     public static boolean generateAssignments(ArrayList<ArrayList<Integer>> clauses, int varIndex, int[] assignment) {
         if (varIndex == assignment.length) {
             return evaluateFormula(clauses, assignment);
         }
-        assignment[varIndex] = 0;
-        if (generateAssignments(clauses, varIndex + 1, assignment)) {
-            return true;
+        for (int i = 0; i < 2; i++) {
+            assignment[varIndex] = i;
+            if (generateAssignments(clauses, varIndex + 1, assignment)) {
+                return true;
+            }
         }
-        assignment[varIndex] = 1;
-        return generateAssignments(clauses, varIndex + 1, assignment);
+        return false;
     }
+
     public static boolean evaluateFormula(ArrayList<ArrayList<Integer>> clauses, int[] assignment) {
         for (ArrayList<Integer> clause : clauses) {
             boolean satisfied = false;
